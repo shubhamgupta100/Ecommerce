259b1510e8caeaf0b2d8fc57af800261dc832418
Shubham Gupta <sg350348@gmail.com>
2021-11-11 18:12:12 +0530

    user profile deleted

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -245,6 +245,9 @@ exports.deleteUser = catchAsyncErrors(async (req, res, next) => {
       new ErrorHandler(`User does not exist with Id: ${req.params.id}`, 400)
     );
   }
+  const imageId = user.avatar.public_id;
+
+  await cloudinary.v2.uploader.destroy(imageId);
   await user.remove();
 
   res.status(200).json({
